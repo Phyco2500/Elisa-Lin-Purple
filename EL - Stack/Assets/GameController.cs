a989b005959654ce4ec0ee71984c5be6adeb43a6
BB <129687370+Phyco2500@users.noreply.github.com>
2023-04-21 19:29:41 -0400

    Poly run PY FInisiheds step 20 sdtfajc StAck

---
+++ EL - Stack/Assets/GameController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [Header("cuube oxjvct")]
+    public GameObject currentCube;
+    [Header("text object")]
+    public GameObject lastCube;
+    [Header("current level")]
+    public int Level;
+    [Header("Boolean")]
+    public bool Done;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void NewBlock()
+    {
+        if(lastCube != null)
+        {
+            currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
+                currentCube.transform.position.y,
+                Mathf.Round(currentCube.transform.position.z));
+            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                                              lastCube.transform.localScale.y,
+                                                              lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+            currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+        }
+    }
+}
